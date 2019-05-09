97ef6758fa18bbc42c7b348b9c200d4073fcf70c
jowd <oj.professionnel.com>
2019-05-09 19:53:35 +0200

    added github finder profile app project

---
+++ js/main.js
@@ -0,0 +1,87 @@
+$(document).ready(function() {
+  $('#searchUser').on('keyup', function(e) {
+    let username = e.target.value;
+
+    //  Make request to Github
+    $.ajax({
+      url: 'https://api.github.com/users/' + username,
+      data: {
+        client_id: '1480fee0b9edc44631c7',
+        client_secret: '8e9f130794efb954e3aacf75b17b5d99a37ed678'
+      }
+    }).done(function(user) {
+      $.ajax({
+        url: 'https://api.github.com/users/' + username + '/repos',
+        data: {
+          client_id: '1480fee0b9edc44631c7',
+          client_secret: '8e9f130794efb954e3aacf75b17b5d99a37ed678',
+          sort: 'created: asc',
+          per_page: 5
+        }
+      }).done(function(repos) {
+        $.each(repos, function(index, repo) {
+          $('#repos').append(`
+        <div class="card">
+          <div class="row">
+            <div class="col-md-7">
+                <strong>${repo.name}</strong>: ${repo.description}
+            </div>
+            <div class="col-md-3">
+                <span class="badge badge-dark">Forks: ${repo.forks_count}</span>
+                <span class="badge badge-light">Watchers: ${
+                  repo.watchers_count
+                }</span>
+                <span class="badge badge-primary">Stars: ${
+                  repo.stargazers_count
+                }</span>
+            </div>
+            <div class="col-md-2">
+                <a href="${
+                  repo.html_url
+                }" target="_blank" class="btn btn-dark">Repo Page</a>
+            </div>
+          </div>
+        </div>
+          `);
+        });
+      });
+
+      $('#profile').html(`
+      <div class="card border-primary mb-3" style="max-width: 100rem;">
+      <div class="card-header"><h3>${user.name}</h3></div>
+      <div class="card-body">
+        <div class="row">
+        <div class="col-md-3">
+          <img class="img-thumbnail avatar" src="${user.avatar_url}">
+          <a target="_blank" class="btn btn-primary btn-block" href="${
+            user.html_url
+          }">View Profile</a>
+        </div>
+        <div class="col-md-9">
+          <span class="badge badge-primary">Public Repos: ${
+            user.public_repos
+          }</span>
+          <span class="badge badge-dark">Public Gists: ${
+            user.public_gists
+          }</span>
+          <span class="badge badge-light">Followers: ${user.followers}</span>
+          <span class="badge badge-light">Following: ${user.following}</span>
+          <br><br>
+          <ul class="list-group">
+            <li class="list-group-item">Company: ${user.company}</li>
+            <li class="list-group-item">Website/blog: <a href="${
+              user.blog
+            }" target="_blank">${user.blog}</a></li>
+            <li class="list-group-item">Location: ${user.location}</li>
+            <li class="list-group-item">Member Since: ${user.created_at}</li>
+          </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <h3 class="page-header">Latest Repos</h3>
+    <div id="repos"></div>
+      `);
+    });
+  });
+});
